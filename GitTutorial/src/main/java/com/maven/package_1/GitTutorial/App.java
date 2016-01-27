a51e1d8c12341ad438b6d7216937d9e3187f559d
technosoftautomation <smchowdhury123@gmail.com>
2016-01-27 17:03:41 -0500

    Added thanks message.

--- GitTutorial/src/main/java/com/maven/package_1/GitTutorial/App.java
+++ GitTutorial/src/main/java/com/maven/package_1/GitTutorial/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Ishrat! How Are your?" );
+
+        System.out.println( "I am fine. Thanks for asking.?" );
     }
 }
